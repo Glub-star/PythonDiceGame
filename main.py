d077553c387a621f43de1a20366595b72f41eae7
Glub-star <135123481+Glub-star@users.noreply.github.com>
2024-09-01 16:27:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ class DiceFace():
     def __init__(self, damage = 0, block = 0):
         self.block = block
         self.damage = damage
-    def ExecuteDice(self, target):
+    def ExecuteDice(self, target, user):
         target.TakeDamage(self.damage)
+        user.block += self.block
+
     
 class Dice():
     def __init__(self):
@@ -15,11 +17,11 @@ class Dice():
         return self.faces[random.randint(0,5)]
 
 class Enemy():
-    def __init__(self):
-        self.health = 50
-        self.maxHealth = 50
-        self.damage = 10
-        self.name = "slime"
+    def __init__(self, maxhealth = 25, name = "slime", damage = 5):
+        self.maxHealth = maxhealth
+        self.health = maxhealth
+        self.damage = damage
+        self.name = name
     def TakeDamage(self, damage):
         self.health -= damage
     def EnemyTurn(self, target):
@@ -32,11 +34,31 @@ class Player():
         self.health = 100
         self.maxHealth = 100
         self.Dice = [*dice]
+        self.alive = True
+        self.score = 0
+        self.block = 0
     
     def TakeDamage(self, damage):
-        self.health -= damage
+        # Apply block to damage first
+        if self.block > 0:
+            if damage >= self.block:
+                damage -= self.block
+                self.block = 0  # Block is used up
+            else:
+                self.block -= damage
+                damage = 0  # All damage was absorbed by the block
+
+        # Apply remaining damage to health
+        if damage > 0:
+            self.health -= damage
+
+            if self.health > self.maxHealth:
+                self.health = self.maxHealth
+        self.DeathCheck()
     
     def Turn(self, enemy):
+        if self.health >= 0 :
+            pass
         # Display the dice results
         diceResults = [dice.Roll() for dice in self.Dice]
         while True:
@@ -53,7 +75,7 @@ class Player():
                     break
                 diceIndex = int(choice)
                 if 0 <= diceIndex < len(diceResults):
-                    diceResults[diceIndex].ExecuteDice(enemy)
+                    diceResults[diceIndex].ExecuteDice(enemy, self)
                     diceResults.pop(diceIndex)
                 else:
                     print("That dice doesn't exist.")
@@ -61,8 +83,9 @@ class Player():
                 print("Enter the NUMBER of the dice or 'x' to end the turn.")
             except IndexError:
                 print("Invalid input. Please enter a valid dice number or 'x' to end the turn.")
+   
     def DisplayStats(self):
-        print(f"Health : {self.health}/{self.maxHealth}\n")
+        print(f"Health : {self.health}/{self.maxHealth}\nBlock : {self.block}\n")
 
     def ShowDiceValues(self):
         for dice in range(len(self.Dice)):
@@ -73,19 +96,33 @@ class Player():
     def UpgradeDice(self, upgrades):
         upgradePoints = upgrades
         print(f"Pick a dice dice to upgrade (Upgrade Points: {upgradePoints})")
-        self.ShowDiceValues()
+        errorBool = False
+        error = None
         while upgradePoints > 0:
             while True:
+                os.system("cls")
+                if errorBool == True:
+                    print("Please enter a valid number ")
+                self.ShowDiceValues()
+                print(f"You have {upgradePoints} upgrades left! ")
                 diceIndex = input("Enter a dice index ")
                 faceIndex =  input("Enter a dice face index ")
                 try:
                     diceIndex = int(diceIndex)
                     faceIndex = int(faceIndex)
+                    self.Dice[diceIndex].faces[faceIndex].block += 1
+                    self.Dice[diceIndex].faces[faceIndex].damage += 1
+                    errorBool = False
+                    upgradePoints -= 1
+                    break
                 except ValueError:
-                    print("Please enter a number")
-                    
-                
-                
+                    errorBool = True
+                except IndexError:
+                    errorBool = True
+
+    def DeathCheck(self):
+        if self.health <= 0:
+            self.alive = False
 
 class Map():
     def __init__(self):
@@ -98,38 +135,93 @@ class Map():
         4 will be boss
         '''
     def NewRoom(self):
-        self.location = random.randint(0,3)
+        self.location = random.randint(0,2)
+        player.score += 1
+
+        if self.location == 0:
+            EnemyRoom()
+        elif self.location == 1:
+            Pool()
+        elif self.location == 2:
+            Altar()
+            
+def EnemyRoom(EnemyGiven=0):
+    if EnemyGiven == 0:
+        enemy = Enemy()
+    else:
+        enemy = EnemyGiven
+
+    print(f"{enemy.health > 0  and player.alive}")
+
+    while enemy.health > 0 and player.alive:
+        player.Turn(enemy)
+        enemy.EnemyTurn(player)
         
-def EnemyRoom():
-    enemy = Enemy()
-    while enemy.health > 0:
-        enemy.EnemyTurn()
-        player.Turn()
+    if enemy.health < 0:
+        input(f"You killed the {enemy.name}! ")
 
 def Pool():
     while True:
         os.system("cls")
-        choice = input("You see a pool ahead\nDo you dive in?\n1.) Yes\n2.) No")
+        choice = input("You see a pool ahead\nDo you dive in?\n1.) Yes\n2.) No\n")
         try:
             choice = int(choice)
             if choice < 0 < 2:
                 raise ValueError
             break
         except ValueError:
-            print("Selecta a valid option")
-    if choice == 0:
+            print("Select a valid option")
+    if choice == 1:
         reward = random.randint(1,2)
-        if reward == 1:
+        if reward == 2:
             print("You found an upgrade!\n")
-            ###############################
+            input()
+            player.UpgradeDice(random.randint(1,2))
+            
         else:
             print("It was a trap!\nYou took 10 damage!")
-            player.health -= 10
+            player.TakeDamage(10)
+            input()
 
     elif choice == 1:
         print("You leave the pool alone...")
+        input()
 
+def Altar():
+    choice = ""
+    while True:
+        os.system("cls")
+        choice = input("You see an omnious altar ahead...\nDo you place your hand upon it?\n1.)Yes \n2.)No\n ")
+        try:
+            choice = int(choice)
+            if choice == 1 or choice == 2:
+                break
+            else:
+                break
+            
+        except ValueError:
+            print("please enter a valid number")
+    if choice == 1:
+        result = random.randint(0,2)
+        if result == 0:
+            print("You found a new dice! ")
+            player.Dice.append(Dice())
+        if result == 1:
+            print("You found multiple upgrades! ")
+            player.UpgradeDice(5)
+        if result == 2:
+            input("You hear something rumbling from below... ")
+            EnemyRoom(EnemyGiven=Enemy(maxhealth=100, name="curiosity", damage = 10))
 
-player = Player(dice = [Dice(),Dice(),Dice()])
+    else:
+        print("You left the altar alone...\nMaybe the better choice\n")
+    input()
 
-player.ShowDiceValues()
+###################################################
+map = Map()
+player = Player(dice = [Dice()])
+
+while player.health > 0:
+    map.NewRoom()
+if player.alive == False:
+    print(f"You have died!\nYour score was {player.score}")
